bc34014e77ce7d854c759e2ab78caf41f548740f
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-08-21 12:44:25 +0200

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,11 +11,11 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 25,
         color: '#010101',
       }}
     >
