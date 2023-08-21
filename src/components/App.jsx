eb3ffceb35dfea7fccb51b9d43ec9b5e9ebd1e29
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-08-21 18:55:57 +0200

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from './Profile/user.json';
-import data from './Statistics/data.json';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
