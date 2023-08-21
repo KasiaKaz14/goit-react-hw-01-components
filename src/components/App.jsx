66fb85e671c933f939641aed65525a193610bc9a
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-08-21 19:02:04 +0200

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,6 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
       <FriendList friends={friends} />;
       <TransactionHistory items={transactions} />;
     </div>
